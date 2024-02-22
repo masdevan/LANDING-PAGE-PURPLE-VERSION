edcd06b6a3d2cf5e44d07be3cd9afe26e80da034
masdevan <81233367+masdevan@users.noreply.github.com>
2024-02-22 12:11:07 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lunox - Personal Page</title>
+    <!--[ Data Library ]-->
+    <link href="CSS/style.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="javascript/icon.js"></script>
+</head>
+<body>
+    <!--[ Header Menu Manufacture ]-->
+    <header class="outer" id="header">
+        <div class="limiter">
+            <div class="headerFlex">
+        <!--[ Header Menu Section ]-->
+        <div class="items1" id='first'>
+            <a href="#"><img src="images/logo.png"></a>
+        </div>
+        <!--[ Header Menu List ]-->
+        <div class="items2" id="second">
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Intro</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Skills</a></li>
+                    <li><a href="#">Help</a></li>
+                </ul>
+            </nav>
+        </div>
+        <!--[ Header menu Search Form ]-->
+        <div class="items3" id="thirty">
+        <form method="get" class="search">
+            <input type="text" placeholder="Search...">
+            <button type="submit"><i class="fa fa-magnifying-glass"></i></button>
+        </input>
+        </form>
+        </div>
+
+        <!--[ Login Form Logo Manufacturer ]-->
+        <div class="items4" id="fourthy">
+            <a href="#"><i class="fa fa-user"></i></a>
+        </div>
+
+            </div>
+        </div>
+    </header>
+    <div class="header-suspension"></div>
+
+    <!--[ Big Section ]-->
+    <div class="bigBoxOuter">
+        <div class="bigBoxLimiter">
+        <!--[ Split Left ]-->
+        <div class="bigBoxSplit">
+            <div class="splitBoxLimiter">
+                <h2>Hai Semua Nama Saya Devan</h2>
+                <sub>Status Saya Adalah Pelajar. dan Saya Sangat Menyukai Kode Yang susah dimengerti para human biasa. juga saya menyukai hal yang menantang. dan ini adalah portofolio saya "Maaf Pemula". Mohon dimaklumi jika ada kesalahan disini. selamat berkunjung dan selamat menikmati Terimakasih.</sub>
+                <button class="first">View More</button>
+                <button class="secondary">Contact Us</button>
+            </div>
+        </div>
+        <!--[ Split Right ]-->
+        <div class="bigBoxSplit">
+            <div class="splitBoxLimiter">
+                
+            <div class="specialBox">
+            <img src='images/profil.png'>
+            <i class='fa fa-gear'></i>
+            <i class='fa fa-cube'></i>
+            <i class='fa fa-microchip'></i>
+            <i class='fa fa-filter'></i>
+            <i class='fa fa-bug'></i>
+            </div>
+
+            </div>
+        </div>
+        </div>
+    </div>
+
+    <!--[ Basic Info Main Box ]-->
+    <div class="basicOuter">
+        <div class="basicLimiter">
+            <h3>My Basic Info</h3>
+            <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam fuga necessitatibus quia libero vel hic.</strong>
+            <!--[ Basic First ]-->
+            <div class="basicFlex">
+            <div class="basic1">
+            <i class="fa fa-server"></i>
+            <h4>Lorem, ipsum dolor.</h4>
+            <span>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel dolore obcaecati officiis suscipit iure. Itaque veritatis eos nemo tenetur magnam rem velit et repellendus corporis!
+            </span>
+            </div>
+            <!--[ Basic Second ]-->
+            <div class="basic2">
+            <i class="fa fa-users-gear"></i>
+            <h4>Lorem, ipsum dolor.</h4>
+            <span>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim officia quod quo sapiente reiciendis aspernatur facilis autem harum tempore beatae tempora culpa, error eligendi libero natus atque. Facere, modi impedit?
+            </span>
+            </div>
+            <!--[ Basic Thirty ]-->
+            <div class="basic3">
+            <i class="fa fa-brain"></i>
+            <h4>Lorem, ipsum dolor.</h4>
+            <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate delectus voluptas numquam vitae aperiam, magni, blanditiis quibusdam saepe explicabo expedita esse, dignissimos velit cumque unde!
+            </span>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <!--[ Brands Box Manufacture ]-->
+    <div class="brandsOuter">
+        <div class="brandsLimiter">
+            <div class="brandsLimit">
+            <h3>Lorem, ipsum dolor.</h3>
+            <strong>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, quae ipsam corporis nostrum distinctio magni atque totam enim quisquam incidunt?</strong>
+            <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis cumque dolorum impedit dolores recusandae quod repellendus doloremque quae, magni error? Expedita porro in fuga iusto. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, nam.</span>
+            </div>
+        </div>
+    </div>
+
+    <!--[ Skills Main Box Manufacture ]-->
+    <div class="skillOuter">
+        <div class="skillLimiter">
+            <h3>Skills Info</h3>
+            <div class="skillBox">
+                <div class="itemSkill"><div class="nrd"><img src="images/blogger.png"> <a href="#">Blogger</a></div></div>
+                <div class="itemSkill"><div class="nrd"><img src="images/sass.png"> <a href="#">SASS</a></div></div>
+                <div class="itemSkill"><div class="nrd"><img src="images/css.png"> <a href="#">CSS</a></div></div>
+                <div class="itemSkill"><div class="nrd"><img src="images/html.png"> <a href="#">HTML</a></div></div>
+            </div>
+            <h3>Handle Projects</h3>
+            <div class="skillBox">
+            <div class="itemSkill"><div class="nrd"><img src="images/blogger.png"> <a href="#">Blogger</a></div></div>
+                <div class="itemSkill"><div class="nrd"><img src="images/sass.png"> <a href="#">SASS</a></div></div>
+                <div class="itemSkill"><div class="nrd"><img src="images/css.png"> <a href="#">CSS</a></div></div>
+                <div class="itemSkill"><div class="nrd"><img src="images/html.png"> <a href="#">HTML</a></div></div>
+            </div>
+        </div>
+    </div>
+
+    <!--[ Footer Strcuture Manufacture ]-->
+    <footer class="footers">
+        <div class="footerLimiter">
+
+        </div>
+    </footer>
+
+</body>
+</html>
